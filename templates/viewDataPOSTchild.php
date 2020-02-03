74f3367dea955a5dae5e1c7160852a672cfaceb4
Lea Cerron <15lc50@queensu.ca>
2020-02-03 18:52:21 -0500

    view data finished, csv and delete buttons not workinf

--- templates/viewDataPOSTchild.php
+++ templates/viewDataPOSTchild.php
@@ -1,13 +1,16 @@
 <?php
+include 'ChromePhp.php';
 require_once('config.php');
-$searchQuery  = "SELECT id, first_name, last_name, age FROM users LIMIT 400" ;  //limiting helps with that memory overflow error in my original question
+
+$searchQuery  = "SELECT id, first_name, last_name, age FROM users WHERE first_name = '" . $_POST['value'] . "' LIMIT 400" ;  //limiting helps with that memory overflow error in my original question
+
 $searchResult = mysqli_query($conn, $searchQuery);
 
 while ($row = mysqli_fetch_row($searchResult)) {
 
     $item = array();
     
-        $item["first_name"] = $row[1];
+       $item["first_name"] = $row[1];
         $item["last_name"] = $row[2];
         $item["age"] = $row[3];
     
@@ -17,4 +20,5 @@ while ($row = mysqli_fetch_row($searchResult)) {
         }
     
 $out = array('aaData' => $output);
+//ChromePhp::log(json_encode($out));
 echo json_encode($out);
